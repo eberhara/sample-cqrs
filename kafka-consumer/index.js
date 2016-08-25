e66efc3e0dc17107f2b759c6f5fb5732d3c493b9
Eberhardt, Andre <santos.eberhardt@gmail.com>
2016-08-25 11:04:16 -0300

    Solving linter findings

--- kafka-consumer/index.js
+++ kafka-consumer/index.js
@@ -1,38 +1,33 @@
-'use strict';
+const Kafka = require("kafka-node");
 
-var kafka = require('kafka-node');
-var Consumer = kafka.Consumer;
-var Offset = kafka.Offset;
-var Client = kafka.Client;
-var topic = 'topic1';
+const client = new Kafka.Client("localhost:2181");
+const topics = [{ topic: "topic1", partition: 0 }];
+const options = { autoCommit: false, fetchMaxWaitMs: 1000, fetchMaxBytes: 1024 * 1024 };
 
-var client = new Client('localhost:2181');
-var topics = [
-    {topic: topic, partition: 0}
-];
-var options = { autoCommit: false, fetchMaxWaitMs: 1000, fetchMaxBytes: 1024 * 1024 };
+const consumer = new Kafka.Consumer(client, topics, options);
+const offset = new Kafka.Offset(client);
 
-var consumer = new Consumer(client, topics, options);
-var offset = new Offset(client);
-
-consumer.on('message', function (message) {
-  console.log(message);
+consumer.on("message", (message) => {
+    console.log(message);
 });
 
-consumer.on('error', function (err) {
-  console.log('error', err);
+consumer.on("error", (err) => {
+    console.log("error", err);
 });
 
 /*
 * If consumer get `offsetOutOfRange` event, fetch data from the smallest(oldest) offset
 */
-consumer.on('offsetOutOfRange', function (topic) {
-  topic.maxNum = 2;
-  offset.fetch([topic], function (err, offsets) {
-    if (err) {
-      return console.error(err);
-    }
-    var min = Math.min(offsets[topic.topic][topic.partition]);
-    consumer.setOffset(topic.topic, topic.partition, min);
-  });
+consumer.on("offsetOutOfRange", (t) => {
+    const topic = t;
+    topic.maxNum = 2;
+
+    offset.fetch([topic], (err, offsets) => {
+        if (err) {
+            console.error(err);
+        }
+
+        const min = Math.min(offsets[topic.topic][topic.partition]);
+        consumer.setOffset(topic.topic, topic.partition, min);
+    });
 });
