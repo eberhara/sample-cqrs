b3e23c5b6f41a3ff74e3074ca03deea550bb01a4
Eberhardt, Andre <santos.eberhardt@gmail.com>
2016-08-24 23:25:13 -0300

    Adding linter

--- kafka-producer/index.js
+++ kafka-producer/index.js
@@ -1,23 +1,27 @@
 
-const kafka = require("kafka-node");
+const Kafka = require("kafka-node");
 
-const producer = new kafka.Producer(new kafka.Client("localhost:2181"), { requireAcks: 1 });
+// Creating connection to kafka broker
+const producer = new Kafka.Producer(new Kafka.Client("localhost:2181"), { requireAcks: 1 });
 const topic = "topic1";
+const partition = 0;
+const attributes = 0;
+
+const event = {
+    batata: 1,
+    laranja: 2,
+};
 
 producer.on("ready", () => {
-    const message = "a message";
-    const keyedMessage = new kafka.KeyedMessage("keyed", "a keyed message");
-
-    const event = {
+    const message = {
         topic,
-        partition: 0,
-        messages: [message, keyedMessage],
-        attributes: 0,
+        partition,
+        messages: JSON.stringify(event),
+        attributes,
     };
 
     producer.send(
-        [event]
-        , (err, result) => {
+        [message], (err, result) => {
             console.log(err || result);
             process.exit();
         }
