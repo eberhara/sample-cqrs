9c61ae62f38a576b676252bbbc4a0913a12e879d
Andre Eberhardt <santos.eberhardt@gmail.com>
2016-10-02 13:28:35 -0300

    Addint get to query-service to webapp

--- webapp/src/components/ListTodos.js
+++ webapp/src/components/ListTodos.js
@@ -14,7 +14,6 @@ class ListTodos extends React.Component {
         fetch('http://localhost:3100/todos', { method: 'GET' }
             ).then((response) => response.json()
             ).then((result) => {
-                console.log(result);
                 setTimeout(this.fetchTodos.bind(this), 3000);
                 this.setState({ todos: result });   
             })
@@ -25,10 +24,21 @@ class ListTodos extends React.Component {
         this.fetchTodos();
     }
 
+    renderTodo(todo){
+        return (
+            <li key={todo.id}>
+                <span className="todo-item-id">{todo.id.substr(0,8)}:</span>
+                <span className="todo-item-text">{todo.text}</span>
+            </li>
+        );
+    }
+
     render() {
-        const list = this.state.todos.map(todo => {
-            return (<div>{todo.id} - {todo.text}</div>)
-        });
+        const list = (
+            <ul>
+                {this.state.todos.map(this.renderTodo)}
+            </ul>
+        );
 
         return (
             <div>
